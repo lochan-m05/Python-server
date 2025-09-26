e24bd81cf2a99220ca7bc4cd4ae006b7f63f80d6
lochan <m.lochan11@outlook.in>
2025-09-26 14:54:19 +0530

    thread have installed next start fun should be written

--- server.py
+++ server.py
@@ -7,3 +7,15 @@ ADDR= (SERVER,PORT)
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(ADDR)
+
+
+def handel_client() :
+    pass
+
+
+def start() :
+    pass 
+
+
+print("[STARTING] Server is Sarting...")
+start()
