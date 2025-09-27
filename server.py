4d33f63a7031ccc184bd1d58cc5776987db17a30
lochan <m.lochan11@outlook.in>
2025-09-27 20:42:34 +0530

    handel_client is committed

--- server.py
+++ server.py
@@ -1,25 +1,35 @@
 import socket
 import threading
 
+HEADER=64
 PORT=8080
 SERVER=socket.gethostbyname(socket.gethostname())
 ADDR= (SERVER,PORT)
+FORMAT="utf-8"
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(ADDR)
 
 
-def handel_client() :
-    pass
+def handel_client (conn, addr) :
+   print(f"[New Connection] {addr} Connected")
+   connected = True
+   while connected :
+       msg=conn.recv(HEADER).decode(FORMAT)
+       
    
 
+   
 def start() :
+
     server.listen() 
     while True :
-        print("[Listening] Server is listenign ")
+        print("[Listening] Server is listening ")
         conn , addr = server.accept()
         thread = threading.Thread(target=handel_client,args=(conn,addr))
         thread.start()
+        print(f"[Active Connection] {threading.active_Count()} -1")
+
         
 
 
