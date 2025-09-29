ca2c539f78bffc4164f11bc078b6b83d88e7755c
lochan <m.lochan11@outlook.in>
2025-09-29 21:28:49 +0530

    server fully done

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ PORT=8080
 SERVER=socket.gethostbyname(socket.gethostname())
 ADDR= (SERVER,PORT)
 FORMAT="utf-8"
+DISCONNECT_MESSAGE = "!Disconnect"
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -18,16 +19,17 @@ def handel_client (conn, addr) :
        msg_len=conn.recv(HEADER).decode(FORMAT)
        msg_len=int(msg_len)
        msg=conn.recv(msg_len).decode(FORMAT)
+       if msg == DISCONNECT_MESSAGE :
+           connected = False
        print(f"[{addr}]  {msg}")
-       
-   
-
+   conn.close()
+    
    
 def start() :
 
     server.listen() 
     while True :
-        print("[Listening] Server is listening ")
+        print(f"[Listening] Server is listening on {SERVER}")
         conn , addr = server.accept()
         thread = threading.Thread(target=handel_client,args=(conn,addr))
         thread.start()
